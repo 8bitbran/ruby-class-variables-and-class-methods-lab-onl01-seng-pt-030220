e15b93de1f5584024d1dc8519a769f85b324c6f6
Brandon Marrero <brandonmar97@gmail.com>
2020-03-19 23:36:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,14 +3,32 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
+  @@artists = []
   
   def initialize(name, artist, genre)
     @@count += 1
+    @@genres << genre
+    @@artists << artist
   end 
   
   def self.count
     @@count
   end
   
+  def self.genres 
+    @@genres.uniq
+  end
   
+  def self.artists 
+    @@artists.uniq
+  end 
+  
+  def self.genre_count 
+    genre_hash = {}
+    @@genres.each do |element|
+      if genre_hash[element]
+        genre_hash[element] + 1
+    end 
+  end 
 end
